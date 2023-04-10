2fe003e43fb41fdb6e1b12f1b7e6ef3930a717de
Kyubin Moon <kyubinann1023@gmail.com>
2023-04-11 02:36:11 +0900

    fix readme.md

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
 
 import React from 'react'
 import FDACovid from "../asset/FDACovid.png"
+import DV1 from "../asset/DV1.png"
+import DV2 from "../asset/DV2.png"
+import DV3 from "../asset/DV3.png"
+import DV4 from "../asset/DV4.png"
+import DV5 from "../asset/DV5.png"
+import DV6 from "../asset/DV6.png"
+import DV7 from "../asset/DV7.png"
+import DV8 from "../asset/DV8.png"
+import '../css/home.css'
 
 const Home = ({username}) => {
   return (
@@ -33,7 +42,15 @@ const Home = ({username}) => {
     <br/><br/><br/>
 
     <div className='subTitle'> Data Visualizations Created by Using the Survey Sample of the Website</div>
-
+    
+    <img className='dataVisImg' src={DV1} alt="DV1"/>
+    <img className='dataVisImg' src={DV3} alt="DV3"/>
+    <img className='dataVisImg' src={DV4} alt="DV4"/>
+    <img className='dataVisImg' src={DV2} alt="DV2"/>
+    <img className='dataVisImg' src={DV5} alt="DV5"/>
+    <img className='dataVisImg' src={DV6} alt="DV6"/>
+    <img className='dataVisImg' src={DV7} alt="DV7"/>
+    <img className='dataVisImg' src={DV8} alt="DV8"/>
 
 
 
